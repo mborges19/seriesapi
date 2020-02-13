82db985eb720f798664bc7e5020c01e87e5e435e
mborges19 <19180121@CFP127ALUNO.LOCAL>
2020-02-13 16:52:16 -0300

    Abilitando Cors dos métodos

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ app.use(express.json())
 
 let alowCrossDomain = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
     res.header('Access-Control-Allow-Headers', '*')
     next()
 }
